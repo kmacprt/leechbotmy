abaf0a25dde3140b0ba509817c134bf50fb5a576
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-09-11 07:50:40 +0530

    Update config.py

--- torleechbot/config.py
+++ torleechbot/config.py
@@ -1,10 +1,14 @@
-    TG_BOT_TOKEN = "1440339475:AAEJgEj1evUN3hg3sTH8kZPB3smAg1OxGpw"
-    # The Telegram API things
-    APP_ID = 1131653685
-    API_HASH = "API_HASH"
-    OWNER_ID ="OWNER_ID"
-    # Get these values from my.telegram.org
-    # to store the channel ID who are authorized to use the bot
-    AUTH_CHANNEL = "-1001335333413"
+from torleechbot.sample_config import Config
+
+class Config(Config):  
+    
+TG_BOT_TOKEN = "1440339475:AAFPffzsysWSV5JIamyAGeuKCm983e-qScY"
+# The Telegram API things
+APP_ID = 1383845
+API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
+OWNER_ID ="1131653685"
+# Get these values from my.telegram.org
+# to store the channel ID who are authorized to use the bot
+AUTH_CHANNEL = "-1001335333413"
     # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
+DOWNLOAD_LOCATION = "./DOWNLOADS"
